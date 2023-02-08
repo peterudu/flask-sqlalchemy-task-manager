cbdf274e84d1f9d19c5c30ef7e9a5a0d134d7d9c
peterudu <puduevborho7@gmail.com>
2023-02-08 20:39:05 +0000

    Add categories

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,7 +5,8 @@ class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+    tasks = db.relationship(
+        "Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
